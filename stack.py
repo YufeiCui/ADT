4431e06adac4509b9d4865d930690f8bc412d193
YufeiC <yufei_cui@hotmail.com>
2017-11-29 22:59:55 -0500

    Added implementation for Stack

--- stack.py
+++ stack.py
@@ -10,17 +10,41 @@ class Stack(object):
     A Stack classes implemented with linked list
     """
 
-    def __init__(self):
-        pass
+    def __init__(self: 'Stack') -> None:
+        self._top = None
 
-    def push(self: 'Stack', data):
-        pass
+    def push(self: 'Stack', data: object) -> None:
+        # create a new Node and set its next to the previous top Node
+        new_node = Node(data)
+        new_node.next = self._top
 
-    def pop(self):
-        pass
+        self._top = new_node
 
-    def is_empty(self):
-        pass
+    def pop(self: 'Stack') -> object:
+        if self.is_empty():
+            raise EmptyStackException
 
-    def peak(self):
-        pass
+        # retrieve the data and point top Node to the next Node in the linked list
+        data = self._top.data
+        self._top = self._top.next
+
+        return data
+
+    def is_empty(self: 'Stack') -> bool:
+        return self._top is None
+
+    def peak(self) -> object:
+        if self.is_empty():
+            raise EmptyStackException
+        else:
+            return self._top.data
+
+    def __repr__(self: 'Stack') -> str:
+        s = ""
+        node = self._top
+
+        while node is not None:
+            s += str(node.data) + ", "
+            node = node.next
+
+        return s[:-2]
